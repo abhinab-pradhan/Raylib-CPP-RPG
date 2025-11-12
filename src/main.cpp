4b9d191519fecea7df5edc1d59508433a84fa1ae
abhinab-pradhan <166309936+abhinab-pradhan@users.noreply.github.com>
2025-11-12 22:54:02 +0530

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,101 @@
+#include <raylib.h>
+#include <raymath.h>
+#define TILE_WIDTH 8
+#define TILE_HEIGHT 8
+#define MAX_TEXTURES 1
+#define WORLD_WIDTH 20
+#define WORLD_HEIGHT 20
+const int screenWidth=800;
+const int screenHeight=600;
+
+typedef struct{
+    int x;
+    int y;
+}sTile;
+
+sTile world[WORLD_WIDTH][WORLD_HEIGHT];
+Camera2D camera={0};
+typedef enum{
+    TEXTURE_TILEMAP=0
+} texture_asset;
+Texture2D textures[MAX_TEXTURES];
+void GameStartup(){
+    InitAudioDevice();
+
+    Image image=LoadImage("assets/colored_tilemap_packed.png");
+    textures[TEXTURE_TILEMAP]=LoadTextureFromImage(image);
+    UnloadImage(image);
+
+    for(int i=0;i<WORLD_WIDTH;i++){
+        for(int j=0;j<WORLD_HEIGHT;j++){
+            world[i][j]=(sTile){
+                .x=i,
+                .y=j,
+            };
+        }
+    }
+
+    camera.target=(Vector2){0,0};
+    camera.offset=(Vector2){(float)screenWidth/2,(float)screenHeight/2};
+    camera.rotation=0.0f;
+    camera.zoom=3.0f;
+
+}
+void GameUpdate(){
+    float wheel=GetMouseWheelMove();
+    if(wheel!=0){
+        const float zoomIncrement=0.125f;
+        camera.zoom+=(wheel*zoomIncrement);
+        if(camera.zoom<3.0f) camera.zoom=3.0f;
+        if(camera.zoom >8.0f) camera.zoom=8.0f;
+    }
+    camera.target=(Vector2){0,0};
+}
+void GameRender(){
+    BeginMode2D(camera);
+    sTile tile;
+    int texture_index_x=0;
+    int texture_index_y=0;
+    for(int i=0;i<WORLD_WIDTH;i++){
+        for(int j=0;j<WORLD_HEIGHT;j++){
+            tile=world[i][j];
+            texture_index_x=4;
+            texture_index_y=4;
+            Rectangle source={(float) texture_index_x * TILE_WIDTH,(float)texture_index_y * TILE_HEIGHT,(float)TILE_WIDTH,(float)TILE_HEIGHT};
+            Rectangle dest={(float)(tile.x * TILE_WIDTH),(float)(tile.y * TILE_HEIGHT),(float)TILE_WIDTH,(float)TILE_HEIGHT};
+            Vector2 origin={0,0};
+            DrawTexturePro(textures[TEXTURE_TILEMAP],source,dest,origin,0.0f,WHITE);
+        }
+    }
+    EndMode2D();
+
+    DrawRectangle(5,5,330,120,Fade(SKYBLUE,0.5f));
+    DrawRectangleLines(5,5,330,120,BLUE);
+
+    DrawText(TextFormat("Camera target: (%06.2f,%06.2f)",camera.target.x,camera.target.y),15,10,14,YELLOW);
+    DrawText(TextFormat("Camera Zoom: %06.2f",camera.zoom),15,30,14,YELLOW);
+}
+void GameShutdown(){
+    for(int i=0;i<MAX_TEXTURES;i++){
+        UnloadTexture(textures[i]);
+    }
+    CloseAudioDevice();
+}
+
+
+
+int main() 
+{
+    InitWindow(screenWidth,screenHeight,"2D Rpg");
+    SetTargetFPS(60);
+
+    GameStartup();
+    while(!WindowShouldClose()){
+        GameUpdate();
+        BeginDrawing();
+        ClearBackground(BLACK);
+        GameRender();
+        EndDrawing();
+    }
+    GameShutdown();
+}
