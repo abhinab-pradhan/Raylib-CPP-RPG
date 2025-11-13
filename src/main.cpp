0c2781ababe3f809bdf7d9d850674adb86aafa4a
abhinab-pradhan <166309936+abhinab-pradhan@users.noreply.github.com>
2025-11-13 22:29:36 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,12 +13,27 @@ typedef struct{
     int y;
 }sTile;
 
+typedef struct 
+{
+    int x;
+    int y;
+} sEntity;
+sEntity player;
 sTile world[WORLD_WIDTH][WORLD_HEIGHT];
 Camera2D camera={0};
 typedef enum{
     TEXTURE_TILEMAP=0
 } texture_asset;
 Texture2D textures[MAX_TEXTURES];
+
+
+void DrawTile(int pos_x,int pos_y,int texture_index_x,int texture_index_y){
+    Rectangle source={(float)TILE_WIDTH * texture_index_x,(float)TILE_HEIGHT*texture_index_y,(float)TILE_WIDTH,(float)TILE_HEIGHT};
+    Rectangle dest={(float)(pos_x),(float)(pos_y),(float)TILE_WIDTH,(float)TILE_HEIGHT};
+    Vector2 origin={0,0};
+    DrawTexturePro(textures[TEXTURE_TILEMAP],source,dest,origin,0.0f,WHITE);
+}
+
 void GameStartup(){
     InitAudioDevice();
 
@@ -35,13 +50,35 @@ void GameStartup(){
         }
     }
 
-    camera.target=(Vector2){0,0};
+    player=(sEntity){
+        .x=TILE_WIDTH*3,
+        .y=TILE_HEIGHT*3
+    };
+
+    camera.target=(Vector2){player.x,player.y};
     camera.offset=(Vector2){(float)screenWidth/2,(float)screenHeight/2};
     camera.rotation=0.0f;
     camera.zoom=3.0f;
 
 }
+
+
 void GameUpdate(){
+    float x=player.x;
+    float y=player.y;
+    if(IsKeyPressed(KEY_LEFT)){
+        x-=1*TILE_WIDTH;
+    }
+    else if(IsKeyPressed(KEY_RIGHT)){
+        x+=1*TILE_WIDTH;
+    }
+    else if (IsKeyPressed(KEY_UP)){
+        y-=1*TILE_HEIGHT;
+    }
+    else if(IsKeyPressed(KEY_DOWN)){
+        y+=1*TILE_HEIGHT;
+    }
+    
     float wheel=GetMouseWheelMove();
     if(wheel!=0){
         const float zoomIncrement=0.125f;
@@ -49,8 +86,13 @@ void GameUpdate(){
         if(camera.zoom<3.0f) camera.zoom=3.0f;
         if(camera.zoom >8.0f) camera.zoom=8.0f;
     }
-    camera.target=(Vector2){0,0};
+
+    player.x=x;
+    player.y=y;
+    camera.target=(Vector2){player.x,player.y};
 }
+
+
 void GameRender(){
     BeginMode2D(camera);
     sTile tile;
@@ -61,12 +103,11 @@ void GameRender(){
             tile=world[i][j];
             texture_index_x=4;
             texture_index_y=4;
-            Rectangle source={(float) texture_index_x * TILE_WIDTH,(float)texture_index_y * TILE_HEIGHT,(float)TILE_WIDTH,(float)TILE_HEIGHT};
-            Rectangle dest={(float)(tile.x * TILE_WIDTH),(float)(tile.y * TILE_HEIGHT),(float)TILE_WIDTH,(float)TILE_HEIGHT};
-            Vector2 origin={0,0};
-            DrawTexturePro(textures[TEXTURE_TILEMAP],source,dest,origin,0.0f,WHITE);
+            DrawTile(tile.x*TILE_WIDTH,tile.y*TILE_HEIGHT,texture_index_x,texture_index_y);
         }
     }
+
+    DrawTile(camera.target.x,camera.target.y,4,0);
     EndMode2D();
 
     DrawRectangle(5,5,330,120,Fade(SKYBLUE,0.5f));
@@ -75,6 +116,8 @@ void GameRender(){
     DrawText(TextFormat("Camera target: (%06.2f,%06.2f)",camera.target.x,camera.target.y),15,10,14,YELLOW);
     DrawText(TextFormat("Camera Zoom: %06.2f",camera.zoom),15,30,14,YELLOW);
 }
+
+
 void GameShutdown(){
     for(int i=0;i<MAX_TEXTURES;i++){
         UnloadTexture(textures[i]);
